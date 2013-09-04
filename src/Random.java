5795072f600c953e03737004eb1d6d428a416218
Alexey Bychko <abychko@gmail.com>
2013-09-04 20:27:04 +0700

    fixed NumberFormat Exception

--- src/Random.java
+++ src/Random.java
@@ -12,14 +12,14 @@ public class Random {
         int myDigit = (int)( Math.random() * 100);
         int trycount = 0;
         String val;
-        int input = -1;
+        int input;
         System.out.println("Enter your Integer");
         while (trycount < 8){
 
             try {
                 val = new Scanner(System.in).next();
                 input = Integer.valueOf(val);
-            }catch (Throwable t){
+            }catch (NumberFormatException t){
                 System.out.println("Not a integer! " + t);
                 continue;
             }
